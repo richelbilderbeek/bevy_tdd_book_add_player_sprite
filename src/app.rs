52fa85d620636466d2015f1c0800f520ad4ac940
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-07-14 12:45:07 +0200

    Describe better what the function does

--- src/app.rs
+++ src/app.rs
@@ -46,7 +46,7 @@ fn get_player_position(app: &mut App) -> Vec2 {
 }
 
 #[cfg(test)]
-fn get_player_scale(app: &mut App) -> Vec2 {
+fn get_player_size(app: &mut App) -> Vec2 {
     let mut query = app.world_mut().query::<(&Transform, &Player)>();
     let (transform, _) = query.single(app.world());
     transform.scale.xy()
@@ -94,7 +94,7 @@ mod tests {
         let initial_player_scale = Vec2::new(64.0, 32.0);
         let mut app = create_app(initial_player_position, initial_player_scale);
         app.update();
-        assert_eq!(get_player_scale(&mut app), initial_player_scale);
+        assert_eq!(get_player_size(&mut app), initial_player_scale);
     }
 
 
